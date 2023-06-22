4621bdcf798267d24b72bcca32d33db39f11f4e8
Vlad Epov <vladep622@gmail.com>
2023-06-22 16:43:22 +0300

    first init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,10 @@
+<script>
+</script>
+
+<template>
+  <div class="about">
+    <h2>This is an about page</h2>
+  </div>
+</template>
+
+<style></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script>
+</script>
+
+<template>
+  <main>
+    <h2>This Is The Main Page</h2>
+  </main>
+</template>
+
+<style></style>
