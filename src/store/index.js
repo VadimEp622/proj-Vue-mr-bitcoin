3f7ee3741b106ebee35f1f2666bf57d71a2363fa
Vlad Epov <vladep622@gmail.com>
2023-12-14 14:33:19 +0200

    cleaned up storeOptions

--- src/store/index.js
+++ src/store/index.js
@@ -3,22 +3,6 @@ import contact from './modules/contact.js'
 
 const storeOptions = {
     strict: true,
-    state() {
-        return {
-            // count: 10,
-        }
-    },
-    mutations: {
-        // increment(state) {
-        //     state.count++
-        // },
-        // setCount(state, { val }) {
-        //     state.count = val
-        // }
-    },
-    getters: {
-        // count(state) { return state.count }
-    },
     modules: {
         contact,
     }
