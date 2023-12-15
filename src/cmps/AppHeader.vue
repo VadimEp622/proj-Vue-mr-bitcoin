0ea03c7563aee868fc5cff154bf93cdfe1e3933c
Vlad Epov <vladep622@gmail.com>
2023-12-15 15:50:58 +0200

    added styling variable maps and helpers

--- src/cmps/AppHeader.vue
+++ src/cmps/AppHeader.vue
@@ -4,27 +4,49 @@ export default {
 </script>
 
 <template>
-    <header class="app-header flex space-between align-center">
-        <section class="logo fs34">
+    <header class="app-header flex space-between align-center bg-clr-blue-0 white-space-nowrap">
+        <section class="logo fs34 fw800 clr-gold-0">
             <RouterLink to="/" class="flex"><span class="pd-10">Mr. Bitcoin</span></RouterLink>
         </section>
-        <nav class="flex fs25">
-            <RouterLink to="/"><span>Home</span></RouterLink>
-            <RouterLink to="/trading-data"><span>Trading</span></RouterLink>
-            <!-- <RouterLink to="/about"><span>About</span></RouterLink> -->
-            <RouterLink to="/my-profile"><span>Profile</span></RouterLink>
+        <nav class="fs25 fw600 clr-gray-0">
+            <ul class="flex">
+                <li>
+                    <RouterLink to="/" class="flex pos-rel"><span class="pd-in-10 pd-bl-15">Home</span></RouterLink>
+                </li>
+                <li>
+                    <RouterLink to="/trading-data" class="flex pos-rel"><span class="pd-in-10 pd-bl-15">Trading</span>
+                    </RouterLink>
+                </li>
+                <li>
+                    <RouterLink to="/my-profile" class="flex pos-rel"><span class="pd-in-10 pd-bl-15">Profile</span>
+                    </RouterLink>
+                </li>
+                <!-- <li>
+                    <RouterLink to="/about" class="flex pos-rel"><span class="pd-in-10 pd-bl-15">About</span></RouterLink>
+                </li> -->
+            </ul>
         </nav>
     </header>
 </template>
 
 <style lang="scss" scoped>
 header {
-    background-color: map-get($map-clr, 'main-blue', 'blue-0');
-    white-space: nowrap;
+    & nav a {
+        &:is(.router-link-active, .router-link-exact-active) {
+            &::after {
+                position: absolute;
+                content: "";
+                height: 3px;
+                width: 100%;
+                background-color: #dcdcdc;
+                bottom: 0;
+                left: 0;
+            }
+        }
 
-    & .logo {
-        font-weight: 800;
-        color: map-get($map-clr, 'main-gold', 'gold-0');
+        &:hover {
+            background-color: #858f7e;
+        }
     }
 }
 </style>
