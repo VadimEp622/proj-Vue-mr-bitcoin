8d352857f177b21aa4b7d512208cc85bea45c767
Vlad Epov <vladep622@gmail.com>
2023-12-20 14:19:39 +0200

    added transition animation to contact-list

--- src/views/MyProfile.vue
+++ src/views/MyProfile.vue
@@ -26,7 +26,7 @@ export default {
 <template>
     <section v-if="loggedinUser" class="my-profile">
         <p class="greeting">Hello, <span>{{ loggedinUser.name }}</span></p>
-        <ContactList @remove="removeContact" :contacts="contacts" />
+        <ContactList v-if="contacts" @remove="removeContact" :contacts="contacts" />
     </section>
     <section v-else>
         <p class="text-align-center">No logged-in user</p>
