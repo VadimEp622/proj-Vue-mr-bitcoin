0122394f3331ce5d02ea3a6710adb97252bbdd2f
Vlad Epov <vladep622@gmail.com>
2024-01-01 16:48:41 +0200

    styled responsive main menu further, and added logout in it

--- src/cmps/ResponsiveMainMenu.vue
+++ src/cmps/ResponsiveMainMenu.vue
@@ -3,23 +3,46 @@
         <div class="responsive-page-content-overlay" @click="onSetMainMenu(false)"></div>
         <section class="main-menu-container">
             <section class="main-menu">
-                <section class="logged-in-user-preview">
-                    <p>hi from logged-in-user-preview</p>
+
+                <section v-if="loggedinUser" class="logged-in-user-preview">
+                    <section class="icon-and-name flex align-center">
+                        <section class="user-icon">
+                            <IconHandler :name="ICON_DEFAULT_USER" />
+                        </section>
+                        <section class="user-name">
+                            <p class="capitalize">{{ loggedinUser.name }}</p>
+                        </section>
+                    </section>
+                    <section class="balance">
+                        <p>Balance: 50$</p>
+                    </section>
                 </section>
 
                 <nav>
                     <ul>
                         <li>
-                            <p>route 1</p>
+                            <RouterLink to="/" class="flex align-center pos-rel">
+                                <span>Home</span>
+                            </RouterLink>
                         </li>
                         <li>
-                            <p>route 2</p>
+                            <RouterLink to="/trading-data" class="flex align-center pos-rel">
+                                <span>Trading</span>
+                            </RouterLink>
                         </li>
                         <li>
-                            <p>route 3</p>
+                            <RouterLink to="/my-profile" class="flex align-center pos-rel">
+                                <span>Profile</span>
+                            </RouterLink>
                         </li>
                     </ul>
                 </nav>
+
+                <section>
+                    <section class="logout flex justify-center">
+                        <button @click="onLogout">logout</button>
+                    </section>
+                </section>
 
             </section>
         </section>
@@ -28,6 +51,9 @@
 
 
 <script>
+import { ICON_DEFAULT_USER } from "@/services/icon-handler.service.js"
+import IconHandler from "@/cmps/app-reusable/IconHandler.vue"
+import { RouterLink } from "vue-router"
 
 export default {
     props: { isMainMenuActive: Boolean, required: true },
@@ -36,12 +62,22 @@ export default {
             return {
                 'main-menu-active': this.isMainMenuActive
             }
-        }
+        },
+        loggedinUser() { return this.$store.getters.user },
+        ICON_DEFAULT_USER() { return ICON_DEFAULT_USER }
     },
     methods: {
+        onLogout() {
+            this.$store.dispatch({ type: 'logout' })
+            this.onSetMainMenu(false)
+        },
         onSetMainMenu(booleanState) {
             this.$emit('setMainMenu', booleanState)
         }
+    },
+    components: {
+        IconHandler,
+        RouterLink
     }
 }
 </script>
@@ -80,6 +116,16 @@ export default {
             & .logged-in-user-preview {
                 padding-block: 24px;
                 padding-inline: 12px;
+
+                & .icon-and-name {
+                    gap: 12px;
+                    margin-block-end: 16px;
+
+                    & .user-icon {
+                        width: 61px;
+                        height: 61px;
+                    }
+                }
             }
 
             & nav {
@@ -87,7 +133,15 @@ export default {
                     padding-inline: 12px;
                     border-top: 1px solid #2f3138;
                     border-bottom: 1px solid #000000;
+
+                    & span {
+                        line-height: 2.5em;
+                    }
                 }
+            }
+
+            & .logout {
+                padding-block: 48px;
             }
         }
     }
