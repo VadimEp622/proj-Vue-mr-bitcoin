e7336472d6b8e624ae03131fdcc61d977925ea72
Vlad Epov <vladep622@gmail.com>
2024-01-03 12:42:27 +0200

    improved validation on FormLogin

--- src/cmps/app-reusable/forms/FormLogin.vue
+++ src/cmps/app-reusable/forms/FormLogin.vue
@@ -3,7 +3,7 @@
         :initial-values="initialValues">
         <section class="form-login flex gap-10">
             <section class="name">
-                <Field name="name" placeholder="Enter name" />
+                <Field name="name" type="text" placeholder="Enter name" autocomplete="off" />
                 <section class="error-container">
                     <ErrorMessage class="error clr-gray-0" name="name" />
                 </section>
@@ -20,7 +20,10 @@ import { Form, Field, ErrorMessage } from 'vee-validate'
 import * as yup from 'yup'
 
 const schema = yup.object({
-    name: yup.string().required().min(1).max(20)
+    name: yup.string().required().min(1).max(20).matches(
+        /^([A-Za-z\u00C0-\u00D6\u00D8-\u00f6\u00f8-\u00ff\s]*)$/gi,
+        'Name can only contain Latin letters.'
+    )
 })
 const props = defineProps({
     initialValues: Object
