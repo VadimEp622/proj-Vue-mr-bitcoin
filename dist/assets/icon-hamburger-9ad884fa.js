fc686e9a337ad958646fb46b4e9312e023284965
Vlad Epov <vladep622@gmail.com>
2024-01-19 07:49:22 +0200

    dist

---
+++ dist/assets/icon-hamburger-9ad884fa.js
@@ -0,0 +1,1 @@
+import{o as e,i as t,d as o}from"./index-e67a2f01.js";const n={xmlns:"http://www.w3.org/2000/svg",width:"20",height:"17"},r=o("path",{fill:"#FFF","fill-rule":"evenodd",d:"M0 0h20v3H0zm0 7h20v3H0zm0 7h20v3H0z"},null,-1),s=[r];function c(d,h){return e(),t("svg",n,[...s])}const a={render:c};export{a as default,c as render};
